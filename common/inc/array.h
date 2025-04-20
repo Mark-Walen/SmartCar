48781fab124805a60732c8b80336452788bc30a3
Mark-Walen <mark_walen@qq.com>
2025-04-20 22:30:29 +0800

    feat: create array.h

---
+++ common/inc/array.h
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2025/4/20.
+//
+
+#ifndef SMART_CAR_ARRAY_H
+#define SMART_CAR_ARRAY_H
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#endif //SMART_CAR_ARRAY_H
